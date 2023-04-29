d153d9dbb2c8b0bdf0d8b96805ba06bd6bf0fd2a
Csaba Kászoni <127140185+kaszonics@users.noreply.github.com>
2023-04-29 10:41:26 +0300

    Add files via upload

---
+++ CreditOrganizer/CreditOrganizer.cpp
@@ -0,0 +1,69 @@
+﻿#include "stdafx.h"
+#include "CreditCardAccount.h"
+
+using namespace System;
+using namespace std;
+
+int main(array<System::String ^> ^args)
+{
+    CreditCardAccount^ myAccount; 
+    myAccount = gcnew CreditCardAccount(12345, 2500);
+    myAccount->SetCreditLimit(1000);
+    myAccount->MakePurchase(1000);
+    myAccount->MakeRepayment(700);
+    myAccount->PrintStatement();
+    long num = myAccount->GetAccountNumber();
+    Console::Write("Account number: ");
+    Console::WriteLine(num);
+
+    /*Console::WriteLine("Creating first object");
+    CreditCardAccount^ account1;
+    account1 = gcnew CreditCardAccount(12345, 2000);
+    account1->MakePurchase(300);
+    account1->PrintStatement();
+    Console::WriteLine("\nCreating second object");
+    CreditCardAccount^ account2;
+    account2 = gcnew CreditCardAccount(67890, 5000);
+    account2->MakePurchase(750);
+    account2->PrintStatement();*/
+
+   /* int n = CreditCardAccount::GetNumberOfAccounts();
+    Console::Write("Number of accounts initially: ");
+    Console::WriteLine(n);
+    Console::WriteLine("\nCreating first object");
+    CreditCardAccount^ account1;
+    account1 = gcnew CreditCardAccount(12345, 2000);
+    account1->MakePurchase(300);
+    account1->PrintStatement();
+    Console::WriteLine("\nCreating second object");
+    CreditCardAccount^ account2;
+    account2 = gcnew CreditCardAccount(67890, 5000);
+    account2->MakePurchase(750);
+    account2->PrintStatement();
+    n = CreditCardAccount::GetNumberOfAccounts();
+    Console::Write("\nNumber of accounts now: ");
+    Console::WriteLine(n);*/
+
+   /* int n = CreditCardAccount::GetNumberOfAccounts();
+    Console::Write("Number of account initially:");
+    Console::WriteLine(n);
+    Console::WriteLine("\nCreating first object");
+    CreditCardAccount^ account1;
+    account1 = gcnew CreditCardAccount(12345, 2000);*/
+
+    Console::WriteLine("Creating account object");
+    CreditCardAccount^ account1;
+    account1 = gcnew CreditCardAccount(12345, 2000);
+    Console::WriteLine("\nMaking a purchase (300)");
+    account1->MakePurchase(300);
+    Console::WriteLine("\nMaking a purchase (700)");
+    account1->MakePurchase(700);
+    Console::WriteLine("\nMaking a purchase (500)");
+    account1->MakePurchase(500);
+    Console::WriteLine("\nRedeeming points");
+    account1->RedeemLoyaltyPoints();
+
+    Console::ReadLine();
+
+    return 0;
+}
